000643aa4f5492a8d0fae4e1ebc7acd6616efab5
hide-a1 <hide210g@gmail.com>
2021-06-11 21:22:58 +0900

    init

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.title

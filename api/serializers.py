3ee1e12726289fdf47c81f43ac5e40ed95b87b1d
hide-a1 <hide210g@gmail.com>
2021-06-13 08:03:59 +0900

    add views

--- api/serializers.py
+++ api/serializers.py
@@ -3,20 +3,22 @@ from .models import Task
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'write_only': True, 'required': True }}
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        Token.create(user=user)
+        Token.objects.create(user=user)
         return user
 
-class TaskSerializer(serializers.ModelSerializer):
 
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H-%M", read_only=True)
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H-%M", read_only=True)
+class TaskSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
 
     class Meta:
         model = Task
